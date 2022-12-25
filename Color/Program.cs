335f0c12061662891a8bd5b3651a330e0d4f442c
Александр Полушкин <aa.polushkin@yandex.ru>
2022-12-25 22:23:03 +0300

    Добавьте файлы проекта.

---
+++ Color/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Color
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.Write("Введите название цвета на английском языке: ");
+                string nameColor = Console.ReadLine();
+                
+                if (!string.IsNullOrEmpty(nameColor))
+                {
+                    nameColor = nameColor.Substring(0, 1).ToUpper() + nameColor.Substring(1).ToLower();
+                    bool isConsoleColor = Enum.IsDefined(typeof(ConsoleColor), nameColor);
+
+                    if (isConsoleColor)
+                    {
+                        ConsoleColor color = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), nameColor, ignoreCase: true);
+                        Console.ForegroundColor = color;
+                        Console.WriteLine($"Выбранный вами цвет: {color}");
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Такого цвета нет");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Вы не ввели наименовани цвета");
+                }
+                
+            }while(true);
+        }
+    }
+}
